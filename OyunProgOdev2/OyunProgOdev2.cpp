7d18200a895b76a744f05205a68654aedfb1b0b2
Behlül UYSAL <mockingjaytes@gmail.com>
2021-05-12 22:26:34 +0300

    Initialized project

---
+++ OyunProgOdev2/OyunProgOdev2.cpp
@@ -0,0 +1,91 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "player.h"
+
+int _width = 400;
+int _height = 750;
+float x = 0;
+int _speed = 1000;
+float cerceveSuresi = 1.0f / 60.0f;
+
+
+
+int main()
+{
+    
+   
+   
+    player* player_sprite = new player();
+
+    sf::RectangleShape rectShape({ 400, 50 });
+    rectShape.setFillColor(sf::Color::Green);
+    rectShape.setPosition(0, 700);
+
+    sf::RenderWindow window(sf::VideoMode(_width, _height), "Space Impact");
+    sf::Clock clock;
+    sf::Clock clock_shoot;
+    sf::Time gecenSure;
+    sf::Time gecenSure_shoot;
+    bool first_shoot = true;
+    window.setFramerateLimit(60);
+    while (window.isOpen())
+    {
+        gecenSure += clock.restart();
+        
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        if (gecenSure.asSeconds() >= cerceveSuresi) 
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                player_sprite->rightMove();
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                player_sprite->leftMove();
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+            {
+                if (first_shoot)
+                {
+                    player_sprite->shoot();
+                    clock_shoot.restart();
+                    first_shoot = false;
+                }
+                else
+                {
+                    gecenSure_shoot += clock_shoot.restart();
+                    if (gecenSure_shoot.asSeconds() > 2)
+                    {
+                        player_sprite->shoot();
+                        gecenSure_shoot -= gecenSure_shoot;
+                    }
+                }
+                
+                   
+            }
+
+
+            player_sprite->animController();
+            window.clear(sf::Color::White);
+
+            window.draw(player_sprite->player_sprite);
+            window.draw(rectShape);
+
+            
+            for (int i = 0; i < player_sprite->bullet_count; i++)
+            {
+                window.draw(player_sprite->ui_bullets[i]);
+            }
+            window.display();
+            gecenSure -= gecenSure;
+        }
+        
+    }
+    return 0;
+}
---
+++ OyunProgOdev2/player.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+class player
+{
+public:
+	int anim;
+	sf::Sprite player_sprite;
+	sf::Sprite ui_bullets[8];
+	int x_val;
+	int y_val;
+	int bullet_count;
+	int hp;
+	sf::Texture playerTextures[8];
+	sf::Texture bullet;
+	player()
+	{
+		x_val = 175;
+		y_val = 650;
+		anim = 0;
+		bullet_count = 8;
+		hp = 3;
+		playerTextures[0].loadFromFile("resimler/uzay/user/1.png", { 26, 26, 448, 448 });
+		playerTextures[1].loadFromFile("resimler/uzay/user/2.png", { 26, 26, 448, 448 });
+		playerTextures[2].loadFromFile("resimler/uzay/user/3.png", { 26, 26, 448, 448 });
+		playerTextures[3].loadFromFile("resimler/uzay/user/4.png", { 26, 26, 448, 448 });
+		playerTextures[4].loadFromFile("resimler/uzay/user/5.png", { 26, 26, 448, 448 });
+		playerTextures[5].loadFromFile("resimler/uzay/user/6.png", { 26, 26, 448, 448 });
+		playerTextures[6].loadFromFile("resimler/uzay/user/7.png", { 26, 26, 448, 448 });
+		playerTextures[7].loadFromFile("resimler/uzay/user/8.png", { 26, 26, 448, 448 });
+
+		bullet.loadFromFile("resimler/uzay/bullet.png", { 46, 26, 36, 66 });
+
+		int ui_bullet_pos = 260;
+		for (int i = 0; i < 8; i++)
+		{
+			ui_bullets[i].setScale(0.4f,0.4f);
+			ui_bullets[i].setTexture(bullet);
+			ui_bullets[i].setPosition(ui_bullet_pos, 710);
+			ui_bullet_pos += 15;
+		}
+	}
+	void animController()
+	{
+		if (anim != 7)
+			anim++;
+		else
+			anim = 0;
+		player_sprite.setTexture(playerTextures[anim]);
+		//448-46 = 422 texture width height,
+		// 422/100 = 4,22 and 50/4,22 = 11,84
+		player_sprite.setScale(0.1184f,0.1184f);
+		player_sprite.setPosition(x_val, y_val);
+	}
+	void leftMove()
+	{
+		if (x_val > 1)
+			x_val -= 6;
+	}
+	void rightMove()
+	{
+		if (x_val < 343)
+			x_val += 6;
+	}
+	void shoot()
+	{
+		bullet_count--;
+	}
+};
+
